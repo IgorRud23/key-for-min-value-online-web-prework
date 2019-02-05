a5168213ef1cfa9a349e5a446d6246f57d114da5
Igor Rud <rud.igor23@gmail.com>
2019-02-05 01:00:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-home = {:pappy => 1, :cats => 6, :birds => 3}
-def key_for_min_value(home)
+
+def key_for_min_value(name_hash)
 lowest_value = 0
 lowest_key = nil
-home.each do |name, age|
+home.each do |key, value|
   if lowest_value == 0 || value < lowest_value
     lowest_value = value
     lowest_value = key
